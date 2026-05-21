99ad38d7952bbd40544514f381aefac7fc0aa7a3
TheNaFeR02 <acunafer.02@gmail.com>
2026-05-20 23:27:00 -0500

    better-auth installation with new auth sqlite db and a cool gear icon with animation.

--- apps/web/app/components/Editor.tsx
+++ apps/web/app/components/Editor.tsx
@@ -28,9 +28,20 @@ export default function Editor() {
 
   return (
     <div className="editor-wrapper">
-      <header >
-        <span>Status: {status}</span>
-        <span>{users.length} online</span>
+      <header className='navbar'>
+        <div>
+          <span>Status: {status} </span>
+          <span>{users.length} online</span>
+        </div>
+        <div className=''>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32">
+            <g className="gear-wrap">
+              <rect width="32" height="32" fill="transparent" />
+              <path fill="#b6b5b5" fillRule="evenodd" d="M13.003,29.003v-4h-2v2h-4v-2h-2v-4h2v-2h-4v-6h4v-2h-2v-4h2v-2h4v2h2v-4h6v4h2v-2h4v2h2v4h-2v2h4v6h-4v2h2v4h-2v2h-4v-2h-2v4H13.003z M20.003,20.003v-7.999h-8.001v7.999H20.003z" clipRule="evenodd" />
+              <path fill="#706d67" fillRule="evenodd" d="M13.003,23.003v-2h-2v-2h-2v-6h2v-2h2v-2h6v2h2v2h2v6h-2v2h-2v2H13.003z M19.003,19.003v-5.999h-6.001v5.999H19.003z" clipRule="evenodd" />
+            </g>
+          </svg>
+        </div>
       </header>
       <EditorContent editor={editor} className="editor-content" />
     </div>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,6 +7,7 @@ import {
 import Editor from './components/Editor';
 
 export default function Home() {
+  console.log("Hocuspocus URL:", process.env.NEXT_PUBLIC_HOCUSPOCUS_URL);
   
   return (
     <HocuspocusProviderWebsocketComponent url={process.env.NEXT_PUBLIC_HOCUSPOCUS_URL!}>
