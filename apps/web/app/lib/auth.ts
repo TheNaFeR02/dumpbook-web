6d4f7d4d987ca8355747b0667edfdb4a73d722d1
TheNaFeR02 <acunafer.02@gmail.com>
2026-05-21 21:43:44 -0500

    fix: update database path to use dynamic path for auth.sqlite

--- apps/web/app/lib/auth.ts
+++ apps/web/app/lib/auth.ts
@@ -3,12 +3,11 @@ import Database from "better-sqlite3";
 import fs from "fs";
 import path from "path";
 
-// creates db programatically in /data/auth.sqlite 
 const dbPath = path.join(process.cwd(), "data", "auth.sqlite");
 fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 
 export const auth = betterAuth({
-    database: new Database("./data/auth.sqlite"),
+    database: new Database(dbPath),
     baseURL: process.env.BETTER_AUTH_URL,
     socialProviders: {
         google: {
